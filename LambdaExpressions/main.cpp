fc112bac206525dfe6cc674dafad0028353244ee
Alexander Brooks <alexanderlawbrooks@gmail.com>
2022-08-09 16:00:34 -0700

    finished building out project structure template

---
+++ LambdaExpressions/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  LambdaExpressions
+//
+//  Created by Alexander Brooks on 8/9/22.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}

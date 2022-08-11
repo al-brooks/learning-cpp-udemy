6ee6d13fd7c42b7c832d5e69b67a820fcc92aef0
Alexander Brooks <alexanderlawbrooks@gmail.com>
2022-08-11 13:32:17 -0700

    pointers and references challenge

--- PointersAndReferences/main.cpp
+++ PointersAndReferences/main.cpp
@@ -1,14 +1,57 @@
-//
-//  main.cpp
-//  PointersAndReferences
-//
-//  Created by Alexander Brooks on 8/9/22.
-//
-
 #include <iostream>
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+using
+std::cin,
+std::cout,
+std::endl;
+
+void print(const int *const arr, size_t size);
+int *apply_all(const int *const arr1, size_t arr1_size, const int *const arr2, size_t arr2_size);
+
+void print(const int *const arr, size_t size) {
+    cout << "[ ";
+    for(size_t i{0}; i < size; ++i)
+        cout << arr[i] << " ";
+    cout << "]";
+    cout << endl;
+}
+
+int *apply_all(const int *const arr1, size_t arr1_size, const int *const arr2, size_t arr2_size) {
+    int *new_array {nullptr};
+    new_array = new int[arr1_size * arr2_size];
+    size_t index {0};
+    for(size_t i{0}; i < arr2_size; ++i) {
+        for(size_t j{0}; j < arr1_size; ++j) {
+            cout << arr2[i] << " " << arr1[j] << endl;
+            new_array[index] = arr2[i] * arr1[j];
+            index++;
+        }
+    }
+    return new_array;
+}
+
+int main() {
+    const size_t array1_size{5};
+    const size_t array2_size{3};
+    
+    int array1[] {1,2,3,4,5};
+    int array2[] {10,20,30};
+    
+    cout << "Array 1: ";
+    print(array1, array1_size);
+    
+    cout << "Array 2: ";
+    print(array2, array2_size);
+    
+    int *results = apply_all(array1, array1_size, array2, array2_size);
+    constexpr size_t results_size {array1_size * array2_size};
+    
+    cout << "Result: ";
+    print(results, results_size);
+    
+    delete [] results; // release heap memory allocation
+    
+    cout << endl;
     return 0;
+    
 }
